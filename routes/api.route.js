485c3d89a575aced91917a348254d07e9cce5bc8
Nick Adams <nick.pappas.adams@gmail.com>
2023-09-30 12:57:04 -0700

    added node fetch

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,7 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const fetch = require("node-fetch");
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+router.get("/", async (req, res, next) => {
+  res.send({ message: "Ok api is working 🚀" });
+});
+
+router.get("/data", async (req, res, next) => {
+  const url = "https://jsonplaceholder.typicode.com/users";
+  const data = await fetch(url);
+  const response = await data.json();
+  res.json(response);
+  console.log(response)
 });
 
 module.exports = router;
